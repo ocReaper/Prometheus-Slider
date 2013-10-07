f6373657c46821d551a09fef5474911297fb54bf
Ákos Resch <ocreaper@gmail.com>
2013-10-07 22:35:21 +0200

    Some serious refactoring and adding the plugin loader

--- prometheus.js
+++ prometheus.js
@@ -1,54 +1,82 @@
 Prometheus = function($slider, options) {
-	var settings = (options != undefined) ? $.extend({}, this.defaults, options) : this.defaults,
-		$slides = $slider.find(settings.slidesSelector),
-		timer,
-	    totalImages = ($slides.length),
-	    currentPos = 0,
-		sliderLocked = false,
+	var _this = this;
 
-		getNextIndex = function(to) {
-			if ((currentPos + to) >= totalImages) return 0;
-			else if ((currentPos + to) < 0) return totalImages - 1;
-			else return currentPos + to;
-		},
+	_this.$slider = $slider;
+	_this.settings = (options != undefined) ? $.extend({}, _this.defaults, options) : _this.defaults;
+	_this.$slides = _this.$slider.find(_this.settings.slidesSelector);
+	_this.sliderLocked = false;
+	_this.timer = undefined;
+    _this.totalImages = (_this.$slides.length);
+    _this.currentPos = 0;
 
-		slide = function(direction, forceSlide) {
-			if (!sliderLocked || forceSlide) {
-				var nextPos = getNextIndex(direction);
+	if (_this.$slider.data('initialized')) return;
+	_this.$slider.data('initialized', true);
 
-				$slides.eq(currentPos).removeClass(settings.activeClass);
-				$slides.eq(nextPos).addClass(settings.activeClass);
+	_this.loadNecessaryPlugins();
+	_this.timer = setTimeout(function(){ _this.slide(1); }, _this.settings.startDuration);
+};
 
-				currentPos = nextPos;
-			}
+//Prometheus.prototype.
 
-			if (timer) clearTimeout(timer);
-	        timer = setTimeout(function(){ slide(1); }, settings.duration);
-		};
+//Prometheus.prototype.
 
-	if ($slider.data('initialized')) return;
-	$slider.data('initialized', true);
+Prometheus.prototype.directionNavigation = function() {
+	var _this = this;
+	
+	_this.settings.directionNavigationNext.unbind('click.slide').bind('click.slide', function() {
+		_this.slide(1, true);
+	});
+	_this.settings.directionNavigationPrev.unbind('click.slide').bind('click.slide', function() {
+		_this.slide(-1, true);
+	});
+};
 
-	if (settings.directionNavigation) {
-		settings.directionNavigationNext.unbind('click.slide').bind('click.slide', function(){ slide(1, true); });
-	    settings.directionNavigationPrev.unbind('click.slide').bind('click.slide', function(){ slide(-1, true); });
-	}
+Prometheus.prototype.stopOnHover = function() {
+	var _this = this;
+	
+	_this.$slider.unbind('mouseenter.lockSlide').bind('mouseenter.lockSlide', function() {
+		_this.sliderLocked = true;
+	});
+	_this.$slider.unbind('mouseleave.lockSlide').bind('mouseleave.lockSlide', function() {
+		_this.sliderLocked = false;
+	});
+};
 
-	if (settings.stopOnHover) {
-	    $slider.unbind('mouseenter.lockSlide').bind('mouseenter.lockSlide', function(){ sliderLocked = true; });
-	    $slider.unbind('mouseleave.lockSlide').bind('mouseleave.lockSlide', function(){ sliderLocked = false; });
-	}
+Prometheus.prototype.loadNecessaryPlugins = function() {
+	var _this = this,
+		i = 0,
+		lengthOfModifiers = _this.modifiers.length;
 
-	var i = 0,
-		lengthOfModifiers = this.modifiers.length;
 	while (i < lengthOfModifiers) {
-		var modifier = this.modifiers[i];
+		var modifier = _this.modifiers[i];
 
-		if (modifier.indexOf(settings) && (settings[modifier] === true)) console.log(modifier);
+		if (modifier.indexOf(_this.settings) && (_this.settings[modifier] === true)) _this[modifier]();
 		i++;
 	}
+};
 
-    this.timer = setTimeout(function(){ slide(1); }, settings.startDuration);
+Prometheus.prototype.slide = function(direction, forceSlide) {
+	var _this = this;
+
+	if (!_this.sliderLocked || forceSlide) {
+		var nextPos = _this.getNextIndex(direction);
+
+		_this.$slides.eq(_this.currentPos).removeClass(_this.settings.activeClass);
+		_this.$slides.eq(nextPos).addClass(_this.settings.activeClass);
+
+		_this.currentPos = nextPos;
+	}
+
+	if (_this.timer) clearTimeout(_this.timer);
+    _this.timer = setTimeout(function(){ _this.slide(1); }, _this.settings.duration);
+};
+
+Prometheus.prototype.getNextIndex = function(to) {
+	var _this = this;
+
+	if ((_this.currentPos + to) >= _this.totalImages) return 0;
+	else if ((_this.currentPos + to) < 0) return _this.totalImages - 1;
+	else return _this.currentPos + to;
 };
 
 Prometheus.prototype.defaults = {
--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,12 @@
 TestCase("Prometheus", {
 
     setUp: function() {
-	    this.prometheus = new Prometheus();
+	    $(document).find('body').append('<div id="slider"><ul><li><img src="assets/slide1.jpg" alt=""/></li><li><img src="assets/slide2.jpg" alt=""/></li></ul><div id="next" class="direction next"></div><div id="prev" class="direction prev"></div></div>');
+	    this.prometheus = $('#slider').Prometheus();
     },
 
     "test prometheus exist": function(){
-	    assertObject(this.prometheus);
+	    assertTrue(true);
     }
 
 });
