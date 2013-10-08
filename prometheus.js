e75a573127a1d5262043ecefa213c79c2bdd9d92
Ákos Resch <ocreaper@gmail.com>
2013-10-08 21:45:53 +0200

    Correcting the test and refactoring the code after understanding the working of "this"

--- prometheus.js
+++ prometheus.js
@@ -1,32 +1,58 @@
 Prometheus = function($slider, options) {
 	var _this = this;
 
-	_this.$slider = $slider;
-	_this.settings = (options != undefined) ? $.extend({}, _this.defaults, options) : _this.defaults;
-	_this.$slides = _this.$slider.find(_this.settings.slidesSelector);
-	_this.sliderLocked = false;
-	_this.timer = undefined;
-    _this.totalImages = (_this.$slides.length);
-    _this.currentPos = 0;
+	this.$slider = $slider;
+	this.settings = (options != undefined) ? $.extend({}, this.defaults, options) : this.defaults;
+	this.$slides = this.$slider.find(this.settings.slidesSelector);
+	this.sliderLocked = false;
+	this.timer = undefined;
+    this.totalImages = (this.$slides.length);
+    this.currentPos = 0;
 
-	if (_this.$slider.data('initialized')) return;
-	_this.$slider.data('initialized', true);
+	if (this.$slider.data('initialized')) return;
+	this.$slider.data('initialized', true);
 
-	_this.loadNecessaryPlugins();
-	_this.timer = setTimeout(function(){ _this.slide(1); }, _this.settings.startDuration);
+	this.loadNecessaryPlugins();
+	this.timer = setTimeout(function(){ _this.slide(1); }, this.settings.startDuration);
 };
 
 //Prometheus.prototype.
 
-//Prometheus.prototype.
+Prometheus.prototype.controlNavigation = function() {
+	var _this = this;
+
+	_this.$slider.append('<ul id="pagination"></ul>');
+	for(var i=0;i<_this.totalImages;i++)
+		$('#pagination').append('<li><span>&nbsp;</span></li>');
+
+	Prometheus.prototype.slide = function(direction, forceSlide) {
+		var _this = this;
+
+		if (!_this.sliderLocked || forceSlide) {
+			var nextPos = _this.getNextIndex(direction),
+				$paginationPage = $('#pagination').find('li');
+
+			_this.$slides.eq(_this.currentPos).removeClass(_this.settings.activeClass);
+			_this.$slides.eq(nextPos).addClass(_this.settings.activeClass);
+
+			$paginationPage.eq(_this.currentPos).removeClass(_this.settings.activeClass);
+			$paginationPage.eq(nextPos).addClass(_this.settings.activeClass);
+
+			_this.currentPos = nextPos;
+		}
+
+		if (_this.timer) clearTimeout(_this.timer);
+	    _this.timer = setTimeout(function(){ _this.slide(1); }, _this.settings.duration);
+	};
+};
 
 Prometheus.prototype.directionNavigation = function() {
 	var _this = this;
 	
-	_this.settings.directionNavigationNext.unbind('click.slide').bind('click.slide', function() {
+	this.settings.directionNavigationNext.unbind('click.slide').bind('click.slide', function() {
 		_this.slide(1, true);
 	});
-	_this.settings.directionNavigationPrev.unbind('click.slide').bind('click.slide', function() {
+	this.settings.directionNavigationPrev.unbind('click.slide').bind('click.slide', function() {
 		_this.slide(-1, true);
 	});
 };
@@ -34,23 +60,20 @@ Prometheus.prototype.directionNavigation = function() {
 Prometheus.prototype.stopOnHover = function() {
 	var _this = this;
 	
-	_this.$slider.unbind('mouseenter.lockSlide').bind('mouseenter.lockSlide', function() {
+	this.$slider.unbind('mouseenter.lockSlide').bind('mouseenter.lockSlide', function() {
 		_this.sliderLocked = true;
 	});
-	_this.$slider.unbind('mouseleave.lockSlide').bind('mouseleave.lockSlide', function() {
+	this.$slider.unbind('mouseleave.lockSlide').bind('mouseleave.lockSlide', function() {
 		_this.sliderLocked = false;
 	});
 };
 
 Prometheus.prototype.loadNecessaryPlugins = function() {
-	var _this = this,
-		i = 0,
-		lengthOfModifiers = _this.modifiers.length;
+	var i = 0;
+	while (i < this.modifiers.length) {
+		var modifier = this.modifiers[i];
 
-	while (i < lengthOfModifiers) {
-		var modifier = _this.modifiers[i];
-
-		if (modifier.indexOf(_this.settings) && (_this.settings[modifier] === true)) _this[modifier]();
+		if (modifier.indexOf(this.settings) && (this.settings[modifier] === true)) this[modifier]();
 		i++;
 	}
 };
@@ -58,25 +81,23 @@ Prometheus.prototype.loadNecessaryPlugins = function() {
 Prometheus.prototype.slide = function(direction, forceSlide) {
 	var _this = this;
 
-	if (!_this.sliderLocked || forceSlide) {
-		var nextPos = _this.getNextIndex(direction);
+	if (!this.sliderLocked || forceSlide) {
+		var nextPos = this.getNextIndex(direction);
 
-		_this.$slides.eq(_this.currentPos).removeClass(_this.settings.activeClass);
-		_this.$slides.eq(nextPos).addClass(_this.settings.activeClass);
+		this.$slides.eq(this.currentPos).removeClass(this.settings.activeClass);
+		this.$slides.eq(nextPos).addClass(this.settings.activeClass);
 
-		_this.currentPos = nextPos;
+		this.currentPos = nextPos;
 	}
 
-	if (_this.timer) clearTimeout(_this.timer);
-    _this.timer = setTimeout(function(){ _this.slide(1); }, _this.settings.duration);
+	if (this.timer) clearTimeout(this.timer);
+    this.timer = setTimeout(function(){ _this.slide(1); }, this.settings.duration);
 };
 
 Prometheus.prototype.getNextIndex = function(to) {
-	var _this = this;
-
-	if ((_this.currentPos + to) >= _this.totalImages) return 0;
-	else if ((_this.currentPos + to) < 0) return _this.totalImages - 1;
-	else return _this.currentPos + to;
+	if ((this.currentPos + to) >= this.totalImages) return 0;
+	else if ((this.currentPos + to) < 0) return this.totalImages - 1;
+	else return this.currentPos + to;
 };
 
 Prometheus.prototype.defaults = {
@@ -94,7 +115,8 @@ Prometheus.prototype.defaults = {
 
 Prometheus.prototype.modifiers = [
 	'stopOnHover',
-	'directionNavigation'
+	'directionNavigation',
+	'controlNavigation'
 ];
 
 $.fn.Prometheus = function(options) {
--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,8 @@
 TestCase("Prometheus", {
 
     setUp: function() {
-	    $(document).find('body').append('<div id="slider"><ul><li><img src="assets/slide1.jpg" alt=""/></li><li><img src="assets/slide2.jpg" alt=""/></li></ul><div id="next" class="direction next"></div><div id="prev" class="direction prev"></div></div>');
-	    this.prometheus = $('#slider').Prometheus();
+	    var $slider = $('<div id="slider"><ul><li><img src="assets/slide1.jpg" alt=""/></li><li><img src="assets/slide2.jpg" alt=""/></li></ul><div id="next" class="direction next"></div><div id="prev" class="direction prev"></div></div>');
+	    this.prometheus = $slider.Prometheus();
     },
 
     "test prometheus exist": function(){
